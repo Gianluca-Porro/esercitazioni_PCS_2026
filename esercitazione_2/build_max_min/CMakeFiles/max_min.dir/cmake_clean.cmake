3f6fad5d46694b723501055d893b82b4c79a4579
Gianluca-Porro <s323156@studenti.polito.it>
2026-03-23 14:10:41 +0000

    consegna_es2

---
+++ esercitazione_2/build_max_min/CMakeFiles/max_min.dir/cmake_clean.cmake
@@ -0,0 +1,12 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/max_min.dir/link.d"
+  "CMakeFiles/max_min.dir/max_min.cpp.o"
+  "CMakeFiles/max_min.dir/max_min.cpp.o.d"
+  "max_min"
+  "max_min.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/max_min.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
